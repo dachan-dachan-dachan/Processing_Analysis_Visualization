d7ceac1c6351bdbe4338649ab7fddb3d6ecef63a
dachan-dachan-dachan <105878214+dachan-dachan-dachan@users.noreply.github.com>
2024-02-24 05:32:13 +0900

    Update main.py

--- Fitting_Function/main.py
+++ Fitting_Function/main.py
@@ -12,24 +12,24 @@ def func(x):#両対数における折れ線近似
     else:
         return 35*(x**(-0.62))
 
-pict_name = 'b.png'#背景画像
-x_min = 10
-x_max = 10000
-y_min = 0.1
-y_max = 100
+pict_name = "HOGE.png"#データシートのグラフの画像
+x_min = 0.1
+x_max = 100000
+y_min = 0.01
+y_max = 10
 base = 10#ログの底
 ######################################################
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     #<データシードの画像を背景に設定する>
     background_image = mpimg.imread(pict_name)#画像の読み込み
     fig, ax = plt.subplots()
     ax.set_xlim(0, 100)
     ax.set_ylim(0, 100)
-    ax.set_aspect('equal', adjustable='box')
-    ax.imshow(background_image, extent=[0, 100, 0, 100], aspect='auto', alpha=0.5)#画像を背景に設定
+    ax.set_aspect("equal", adjustable="box")
+    ax.imshow(background_image, extent=[0, 100, 0, 100], aspect="auto", alpha=1)#画像を背景に設定
   
     #axの軸の数値を非表示
     ax.set_xticks([])
@@ -40,17 +40,18 @@ if __name__ == '__main__':
     #<自分で設定した関数を描画する>
     #両対数軸を追加
     ax_log = fig.add_axes(ax.get_position(), frameon=False)
-    ax_log.set_xscale('log',base=base)
-    ax_log.set_yscale('log',base=base)
+    ax_log.set_xscale("log" ,base=base)
+    ax_log.set_yscale("log" ,base=base)
     ax_log.set_xlim(x_min, x_max)
     ax_log.set_ylim(y_min, y_max)
     
     #関数を描写
-    x_values = np.logspace(math.log(x_min, base), math.log(x_max, base), 100000, base=base)
+    N = 100000#分割回数
+    x_values = np.logspace(math.log(x_min, base), math.log(x_max, base), N, base=base)
     y_values = []
     for i in x_values:
         y_values.append(func(i))
-    ax_log.plot(x_values, y_values, label='func', color='blue', linestyle='--')
+    ax_log.plot(x_values, y_values, label="func", color="blue", linestyle="--")
     ax_log.grid(True)
     #</自分で設定した関数を描画する>
     
