5e8e812d4a776dbbd94aa33870f71c2e7acd44c3
dachan-dachan-dachan <105878214+dachan-dachan-dachan@users.noreply.github.com>
2024-02-24 05:09:24 +0900

    Create main.py

---
+++ Fitting_Function/main.py
@@ -0,0 +1,59 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+
+import math
+
+
+######################################################
+def func(x):#両対数における折れ線近似
+    if x < 100:
+        return 101*(x**(-0.85))
+    else:
+        return 35*(x**(-0.62))
+
+pict_name = 'b.png'#背景画像
+x_min = 10
+x_max = 10000
+y_min = 0.1
+y_max = 100
+base = 10#ログの底
+######################################################
+
+
+if __name__ == '__main__':
+
+    #<データシードの画像を背景に設定する>
+    background_image = mpimg.imread(pict_name)#画像の読み込み
+    fig, ax = plt.subplots()
+    ax.set_xlim(0, 100)
+    ax.set_ylim(0, 100)
+    ax.set_aspect('equal', adjustable='box')
+    ax.imshow(background_image, extent=[0, 100, 0, 100], aspect='auto', alpha=0.5)#画像を背景に設定
+  
+    #axの軸の数値を非表示
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.grid(True)
+    #</データシードの画像を背景に設定する>
+
+    #<自分で設定した関数を描画する>
+    #両対数軸を追加
+    ax_log = fig.add_axes(ax.get_position(), frameon=False)
+    ax_log.set_xscale('log',base=base)
+    ax_log.set_yscale('log',base=base)
+    ax_log.set_xlim(x_min, x_max)
+    ax_log.set_ylim(y_min, y_max)
+    
+    #関数を描写
+    x_values = np.logspace(math.log(x_min, base), math.log(x_max, base), 100000, base=base)
+    y_values = []
+    for i in x_values:
+        y_values.append(func(i))
+    ax_log.plot(x_values, y_values, label='func', color='blue', linestyle='--')
+    ax_log.grid(True)
+    #</自分で設定した関数を描画する>
+    
+    
+    # グラフの表示
+    plt.show()
