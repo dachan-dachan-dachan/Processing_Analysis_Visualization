9e2dc96083b666ae970a189eeec0454412acce04
dachan-dachan-dachan <105878214+dachan-dachan-dachan@users.noreply.github.com>
2024-06-19 14:53:33 +0900

    Create main.py

---
+++ Digital_Filter/main.py
@@ -0,0 +1,71 @@
+from matplotlib import pyplot as plt
+import math
+
+
+def low_path_filter(x, a):
+    y = []
+    for i in range(len(x)):
+        if i == 0:
+            tem = x[0]
+        else:
+            tem = a*y[i-1] + (1-a)*x[i]
+        y.append(tem)
+    return y
+
+def high_path_filter(x, a):
+    y = []
+    for i in range(len(x)):
+        if i == 0:
+            tem = x[0]
+        else:
+            tem = ( x[i] - a*x[i-1] )/(1-a)
+        y.append(tem)
+    return y
+
+
+def func(t):
+    a = 10
+    b = 0.1
+    return 0.3*math.sin(a*t) + math.sin(b*t)
+
+t = []
+x = []
+N = 10000
+start = 0
+end = 100
+for i in range(N+1):
+    tem = start + (i/N)*( end - start )
+    t.append(tem)
+    x.append( func(tem) )
+
+a = 0.99
+plt.ylim(-2,2)
+plt.plot(t, x, label="Original")
+
+l = low_path_filter(x, a)
+#l = high_path_filter(x, a)
+#l = low_path_filter(l, a)
+plt.plot(t, l, label="low path filter")
+
+
+l = low_path_filter(l, a)
+plt.plot(t, l, label="2 times low path filter")
+
+"""
+low_path = low_path_filter(x, a)
+#plt.plot(t, low_path, label="low path filter")
+for i in range(10):
+    low_path = low_path_filter(low_path, a)
+plt.plot(t, low_path, label="low path filter")
+"""
+
+"""
+high_path = high_path_filter(x, a)
+#plt.plot(t, high_path, label="high path filter")
+for i in range(2):
+    high_path = high_path_filter(high_path, a)
+plt.plot(t, high_path, label="high path filter")
+"""
+plt.legend()
+plt.show()
+
