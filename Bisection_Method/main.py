96ca5e61dc94d3e9e3a6b866b35844205b4d863e
dachan-dachan-dachan <105878214+dachan-dachan-dachan@users.noreply.github.com>
2024-06-26 09:47:54 +0900

    Update main.py

--- Bisection_Method/main.py
+++ Bisection_Method/main.py
@@ -6,10 +6,10 @@ def func(x):
     return y
 
 
-def x_on_0(func, d_erro, x_min, x_max):
+def x_on_0(func, d_error, x_min, x_max):
     max_calc_times = 10000
     calc_times = 0
-    while ( abs(x_min - x_max) > d_erro ) and (calc_times < max_calc_times):
+    while ( abs(x_min - x_max) > d_error ) and (calc_times < max_calc_times):
         x_center = (x_min + x_max)/2
         y_on_center = func(x_center)
         if y_on_center == 0:
@@ -29,7 +29,7 @@ def x_on_0(func, d_erro, x_min, x_max):
     
     if calc_times == max_calc_times:
         print("規定の計算回数では解が収束しませんでした")
-    print(f"x = {x_center} , erro = {abs(x_min - x_max)}")
+    print(f"x = {x_center} , error = {abs(x_min - x_max)}")
     return x_center
 
 
@@ -37,7 +37,7 @@ def x_on_0(func, d_erro, x_min, x_max):
 x_min = -1
 x_max = 2
 N = 1000
-d_erro = 0.001
+d_error = 0.001
 
 x = []
 y = []
@@ -49,7 +49,7 @@ for i in range(N+1):
 
 
 plt.plot(x, y, marker="p", linestyle="", markersize=3, zorder=1)
-x_on_zero = x_on_0(func, d_erro, x_min, x_max)
+x_on_zero = x_on_0(func, d_error, x_min, x_max)
 plt.plot(x_on_zero, func(x_on_zero), marker="x", linestyle="", markersize=10, zorder=2)
 plt.xlabel("X axis", fontsize=10)
 plt.ylabel("Y axis", fontsize=10)
